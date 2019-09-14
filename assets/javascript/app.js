249d892f2b38061d45260e8fdd5ce9939766c4dc
Rachael Clark <rachael.clark@uconn.edu>
2019-09-14 12:27:05 -0400

    updated styling for returned gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,17 +15,39 @@ $(document).ready(function () {
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
 
-            var animalDiv = $("<div class='animal'>");
-            var rating = response.Rated;
-            var p = $('<p>').text('Rating: ' + rating);
+                var animalDiv = $("<div class='animal'>");
+                var rating = results[i].rating;
+                var p = $('<p>').text('Rating: ' + rating);
 
 
-            var animalImage = $('<img>')
-            animalImage.attr('src', results[i].images.fixed_height.url);
-            animalDiv.append(animalImage);
-            animalDiv.append(p);
-            $('#gifs-here').prepend(animalDiv);
+                var animalImage = $('<img>')
+                animalImage.attr('src', results[i].images.fixed_height.url);
+                animalImage.attr('data-still', results[i].images.fixed_height_still.url);
+                animalImage.attr('data-animate', results[i].images.fixed_height.url);
+                animalImage.attr('data-state', 'animate');
+                animalImage.addClass('gif');
+                animalDiv.append(animalImage);
+                animalDiv.append(p);
+                $('#gifs-here').prepend(animalDiv);
+
+                console.log(response.data);
             }
+
+            $('img').on('click', function () {
+                console.log(this);
+                var state = $(this).attr('data-state');
+        
+                if (state === 'animate') {
+                    $(this).attr('src', $(this).attr('data-still'));
+                    $(this).attr("data-state", "still");
+                }
+                else {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                }
+        
+            });
+
         });
 
     }
@@ -39,7 +61,7 @@ $(document).ready(function () {
 
         for (var i = 0; i < topics.length; i++) {
             var a = $('<button>');
-            a.addClass('animal-btn btn-primary');
+            a.addClass('animal-btn btn btn-primary');
             a.attr('data-animal', topics[i]);
             a.text(topics[i]);
             $('#buttons-view').append(a);
